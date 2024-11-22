834523fc0b6978a93776fab79616f5cb79f0585f
luca823-0326 <lucas314866@gmail.com>
2024-11-22 11:59:15 +0800

    2241885464

---
+++ lsjhs-pano/data - 複製 - 複製.js
@@ -0,0 +1,597 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "麗中校門",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.4815167409364065,
+        "pitch": 0.09993516346392717,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.1858353005008837,
+          "pitch": 0.18592586162454694,
+          "rotation": 0,
+          "target": "5-1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.49042148201801083,
+          "pitch": 0.04904994330681056,
+          "title": "麗山國中校門",
+          "text": "臺北市內湖區內湖路一段629巷42號<br>"
+        }
+      ]
+    },
+    {
+      "id": "1-1",
+      "name": "麗中中庭1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 2.8329669202645498,
+        "pitch": 0.07285838340678552,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.2858870949734875,
+          "pitch": 0.23858397817697075,
+          "rotation": 0,
+          "target": "2--"
+        },
+        {
+          "yaw": 2.059835579488743,
+          "pitch": 0.13479728263135016,
+          "rotation": 6.283185307179586,
+          "target": "3-"
+        },
+        {
+          "yaw": 0.828134268315436,
+          "pitch": 0.029293300090646568,
+          "rotation": 0,
+          "target": "7-"
+        },
+        {
+          "yaw": 2.9940857114685553,
+          "pitch": 0.06884279426358475,
+          "rotation": 0.7853981633974483,
+          "target": "9-"
+        },
+        {
+          "yaw": -1.216141696176269,
+          "pitch": 0.06673677075539786,
+          "rotation": 5.497787143782138,
+          "target": "11-2"
+        },
+        {
+          "yaw": 2.5007923448521865,
+          "pitch": 0.09016162356922308,
+          "rotation": 0,
+          "target": "5-1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.688497685052175,
+          "pitch": 0.10943001497589577,
+          "title": "戶外羽球場",
+          "text": "平時為羽球場和童軍搭工程的地方"
+        }
+      ]
+    },
+    {
+      "id": "2--",
+      "name": "建築-滑索",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.05699034292227978,
+        "pitch": 0.15774418248705047,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.06265174502611792,
+          "pitch": 0.23841541303405123,
+          "rotation": 5.497787143782138,
+          "target": "1-1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.2856145778214376,
+          "pitch": 0.21852399478840745,
+          "title": "童軍工程",
+          "text": "滑索"
+        }
+      ]
+    },
+    {
+      "id": "3-",
+      "name": "麗中童軍團團部入口",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.22797075047762583,
+        "pitch": 0.1348458334163496,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.4514948954088478,
+          "pitch": 0.48091894837134497,
+          "rotation": 0,
+          "target": "4-"
+        },
+        {
+          "yaw": 1.770930092842944,
+          "pitch": 0.5939983454626834,
+          "rotation": 0.7853981633974483,
+          "target": "1-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-",
+      "name": "麗中童軍團團部",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -1.3837299850069442,
+        "pitch": 0.23271582819569758,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.6947255077466394,
+          "pitch": 0.28611079963940966,
+          "rotation": 0,
+          "target": "3-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.5970717387912927,
+          "pitch": 0.43753475113230245,
+          "title": "童軍團不",
+          "text": "待了3年的團部"
+        }
+      ]
+    },
+    {
+      "id": "5-1",
+      "name": "麗中操場1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 2.7973094271065726,
+        "pitch": 0.05828670672545044,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.486705270404732,
+          "pitch": 0.1615471228382468,
+          "rotation": 0,
+          "target": "1-1"
+        },
+        {
+          "yaw": -0.5382181080946822,
+          "pitch": 0.14474277559178006,
+          "rotation": 0,
+          "target": "6-2"
+        },
+        {
+          "yaw": 2.339390413865339,
+          "pitch": 0.14877942311727743,
+          "rotation": 0,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -3.0838170277951065,
+          "pitch": 0.2777124424637556,
+          "title": "操場跑道",
+          "text": "測驗100公尺時所用的跑道"
+        }
+      ]
+    },
+    {
+      "id": "6-2",
+      "name": "麗中操場2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.05164749827330084,
+        "pitch": 0.01017704403142261,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.1976889203097727,
+          "pitch": 0.21224831865329996,
+          "rotation": 0,
+          "target": "5-1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.6909617355996307,
+          "pitch": 0.11031529557367037,
+          "title": "中心球場",
+          "text": "戶外朝會時學生集合的地方，平時為雪生們打籃球的好去處"
+        }
+      ]
+    },
+    {
+      "id": "7-",
+      "name": "麗中停車場",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -2.3547153214789667,
+        "pitch": 0.1092706544894817,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.6137405216259246,
+          "pitch": 0.12084561051795006,
+          "rotation": 0.7853981633974483,
+          "target": "8-"
+        },
+        {
+          "yaw": -2.8699494500994795,
+          "pitch": 0.13311038487729654,
+          "rotation": 4.71238898038469,
+          "target": "1-1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.3580451341600401,
+          "pitch": 0.2591318747660889,
+          "title": "停車場",
+          "text": "我9年級時的掃區"
+        }
+      ]
+    },
+    {
+      "id": "8-",
+      "name": "麗中音樂廳",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -1.094403185885323,
+        "pitch": 0.039474942597365725,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -1.666568818419087,
+          "pitch": 0.26980438115595007,
+          "title": "音樂廳",
+          "text": "主要為音樂表演場地，有時會作為音樂教室"
+        }
+      ]
+    },
+    {
+      "id": "9-",
+      "name": "麗中教室走廊",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -2.1726856200225946,
+        "pitch": 0.1512836399095825,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.8614659168850025,
+          "pitch": 0.24506980236689913,
+          "rotation": 5.497787143782138,
+          "target": "10-911"
+        },
+        {
+          "yaw": 1.45196483602676,
+          "pitch": 0.24817363462992326,
+          "rotation": 0,
+          "target": "1-1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.301281876122503,
+          "pitch": 0.5542517848377546,
+          "title": "教室走廊",
+          "text": "請勿奔跑!!!"
+        }
+      ]
+    },
+    {
+      "id": "10-911",
+      "name": "麗中911教室",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -2.2253800414140237,
+        "pitch": 0.12669135068195203,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.844630552520689,
+          "pitch": 0.14453812158429002,
+          "rotation": 0,
+          "target": "9-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.9678838617789065,
+          "pitch": -0.0849427351719001,
+          "title": "911教室",
+          "text": "在麗山國中最後一年上課的教室"
+        }
+      ]
+    },
+    {
+      "id": "11-2",
+      "name": "麗中中庭2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -3.0552344517617165,
+        "pitch": 0.027464095126362054,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.0164667606874636,
+          "pitch": 0.0918696231777325,
+          "rotation": 0,
+          "target": "1-1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.7047935951909672,
+          "pitch": 0.04781818318921083,
+          "title": "後中庭",
+          "text": "戶外羽球場"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
